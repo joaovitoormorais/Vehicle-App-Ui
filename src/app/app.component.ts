187b442ed786f4ac0854f0fd1869b05c6696240c
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2024-11-05 16:52:18 -0300

    Atualizaçãozinha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component ( {
 selector: 'app-root', 
-  template :"<router-outlet></router-outlet>"
-  
+  template :"<router-outlet></router-outlet>" //é utilizado pra definir um espaço onde os componentes
+                                              //são usados pra definir rotas, estrutura pra navegar
+                                              //entre elas e pode ser usado para múltiplas rotas
 })
 export class AppComponent {
 title = "vehicle-app-ui"
