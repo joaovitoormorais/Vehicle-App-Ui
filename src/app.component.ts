5f184665a16cc7304fe7bc4781e54275a58dfa17
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2024-11-12 11:53:57 -0300

    resolvendo bugs

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component ( {
-selector: 'app-root', 
-  template :"<router-outlet></router-outlet>" //é utilizado pra definir um espaço onde os componentes
+  selector: 'app-root', 
+  template : `<router-outlet></router-outlet>`//é utilizado pra definir um espaço onde os componentes
                                               //são usados pra definir rotas, estrutura pra navegar
                                               //entre elas e pode ser usado para múltiplas rotas
 })
 export class AppComponent {
-title = "vehicle-app-ui"
+title = "vehicle-app-ui";
   
 }
 
